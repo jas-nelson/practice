6ccab04aee2a438b29542fad2e62e438a75160bf
Jason Nelson <jason.nelson@salesforce.com>
2018-08-15 13:14:24 -0400

    add logic for GenerateFile to LargestRectangle.GenerateFile(); create first version of random test file

--- hackerRankTestFileGenerator/Stacks_and_Queues/LargestRectangle.py
+++ hackerRankTestFileGenerator/Stacks_and_Queues/LargestRectangle.py
@@ -1,7 +1,31 @@
+from random import randint
 from Interfaces import RandomRangedValueList
+import logging
+import os
 
 class LargestRectangle_TestFile:
-
+    buildings = None
+    logger = logging.getLogger("LargestRectangle_TestFile_Logger")
 
     def __init__(self):
-        return 
+        self.buildings = RandomRangedValueList.RandomRangedValueList(floor=1,
+                                                                     ceiling=100000,
+                                                                     numOfElem=randint(1, 1000000))
+
+
+    def GenerateFile(self, fileName):
+
+        cd = os.curdir
+        savePath = os.path.join(cd, fileName)
+
+        with open(savePath, "w") as file:
+
+            file.write(str(self.buildings.numOfElements) + "\n")
+            file.write(" ".join(str(x) for x in self.buildings.rangedList))
+
+            file.close()
+
+
+fileName = "largestRectangle_testFile.txt"
+testFile = LargestRectangle_TestFile()
+testFile.GenerateFile(fileName=fileName)
